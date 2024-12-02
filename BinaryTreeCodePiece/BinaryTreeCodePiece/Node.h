2862d982a6b66581dc93479d163f74a491f8f16f
AidenSakat <126438824+AidenSakat@users.noreply.github.com>
2024-12-01 20:29:14 -0800

    Delete BinaryTreeCodePiece/BinaryTreeCodePiece/Node.h

--- BinaryTreeCodePiece/BinaryTreeCodePiece/Node.h
+++
@@ -1,8 +0,0 @@
-struct Node {
-    int value;               // Data stored in the node
-    Node* left;          // Pointer to the left child
-    Node* right;         // Pointer to the right child
-
-    // Constructor
-    Node(int val) : value(val), left(nullptr), right(nullptr) {}
-};
